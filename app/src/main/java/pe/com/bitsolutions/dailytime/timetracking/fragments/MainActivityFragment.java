571c54e3d3ccb667bc9a6b4388744e3c5994a73d
Alberto Borda <betoborda@gmail.com>
2015-10-03 10:06:08 -0500

    Inicializando un ArrayAdapter

--- app/src/main/java/pe/com/bitsolutions/dailytime/timetracking/fragments/MainActivityFragment.java
+++ app/src/main/java/pe/com/bitsolutions/dailytime/timetracking/fragments/MainActivityFragment.java
@@ -1,10 +1,13 @@
 package pe.com.bitsolutions.dailytime.timetracking.fragments;
 
-import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+
+import java.util.List;
 
 import pe.com.bitsolutions.dailytime.timetracking.R;
 
@@ -16,9 +19,22 @@ public class MainActivityFragment extends Fragment {
     public MainActivityFragment() {
     }
 
+    private List<String> generarDatos() {
+        return null;
+    }
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_main, container, false);
+        View root = inflater.inflate(R.layout.fragment_main, container, false);
+
+        ArrayAdapter<String> tareaAdapter = new ArrayAdapter<String>(
+                getActivity(), // contexto
+                R.layout.list_item_tarea, // layout ID
+                R.id.list_item_tarea_textview, // ID del control
+                generarDatos() //
+        );
+
+        return root;
     }
 }
