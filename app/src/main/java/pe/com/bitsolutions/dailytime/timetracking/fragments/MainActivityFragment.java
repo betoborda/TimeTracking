07dc53d2b9431889bf3e7ceaa6205940d1064989
Alberto Borda <betoborda@gmail.com>
2015-10-03 10:19:22 -0500

    Mostrando el ArrayAdapter

--- app/src/main/java/pe/com/bitsolutions/dailytime/timetracking/fragments/MainActivityFragment.java
+++ app/src/main/java/pe/com/bitsolutions/dailytime/timetracking/fragments/MainActivityFragment.java
@@ -6,7 +6,10 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import pe.com.bitsolutions.dailytime.timetracking.R;
@@ -20,13 +23,25 @@ public class MainActivityFragment extends Fragment {
     }
 
     private List<String> generarDatos() {
-        return null;
+        String[] data = new String[11];
+        data[0] = "aa";
+        data[1] = "bb";
+        data[2] = "bb";
+        data[3] = "bb";
+        data[4] = "bb";
+        data[5] = "bb";
+        data[6] = "bb";
+        data[7] = "bb";
+        data[8] = "bb";
+        data[9] = "bb";
+        data[10] = "bb";
+        return new ArrayList<>(Arrays.asList(data));
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View root = inflater.inflate(R.layout.fragment_main, container, false);
+        View fragmento = inflater.inflate(R.layout.fragment_main, container, false);
 
         ArrayAdapter<String> tareaAdapter = new ArrayAdapter<String>(
                 getActivity(), // contexto
@@ -35,6 +50,9 @@ public class MainActivityFragment extends Fragment {
                 generarDatos() //
         );
 
-        return root;
+        ListView listaDeTareas = (ListView) fragmento.findViewById(R.id.listview_tarea);
+        listaDeTareas.setAdapter(tareaAdapter);
+
+        return fragmento;
     }
 }
