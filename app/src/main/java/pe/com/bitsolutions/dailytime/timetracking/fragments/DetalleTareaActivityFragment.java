88da41ad925859511e821a23a83e34621f749e5a
Alberto Borda <betoborda@gmail.com>
2015-10-03 12:03:54 -0500

    Ver detalle de Tarea

--- app/src/main/java/pe/com/bitsolutions/dailytime/timetracking/fragments/DetalleTareaActivityFragment.java
+++ app/src/main/java/pe/com/bitsolutions/dailytime/timetracking/fragments/DetalleTareaActivityFragment.java
@@ -3,19 +3,51 @@ package pe.com.bitsolutions.dailytime.timetracking.fragments;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.view.MenuItemCompat;
+import android.support.v7.widget.ShareActionProvider;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
 import pe.com.bitsolutions.dailytime.timetracking.R;
+import pe.com.bitsolutions.dailytime.timetracking.activities.DetalleTareaActivity;
+import pe.com.bitsolutions.dailytime.timetracking.tools.Logging;
 
 /**
  * A placeholder fragment containing a simple view.
  */
 public class DetalleTareaActivityFragment extends Fragment {
 
+    private String LOG_TAG = DetalleTareaActivity.class.getSimpleName();
+
     public DetalleTareaActivityFragment() {
+        setHasOptionsMenu(true);
+        Logging.v(LOG_TAG, "DetalleTareaActivityFragment");
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+
+        Logging.v(LOG_TAG, "onCreateOptionsMenu");
+        inflater.inflate(R.menu.menu_detalle_tarea_fragment, menu);
+
+        MenuItem menuItem = menu.findItem(R.id.action_compartir);
+        ShareActionProvider comparirProvider = (ShareActionProvider)
+                MenuItemCompat.getActionProvider(menuItem);
+
+        Intent compartirIntent = new Intent(Intent.ACTION_SEND);
+        compartirIntent.setType("text/plain");
+        compartirIntent.putExtra(Intent.EXTRA_TEXT, "cadena para compartir");
+
+        if (comparirProvider != null) {
+            comparirProvider.setShareIntent(compartirIntent);
+        } else {
+            Logging.v(LOG_TAG, "Proveedor para compartir nulo");
+        }
     }
 
     @Override
