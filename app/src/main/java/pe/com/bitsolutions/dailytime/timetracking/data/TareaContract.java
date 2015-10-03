a093b6874c6e9e5380d8dcbbead5adcde92c7754
Alberto Borda <betoborda@gmail.com>
2015-10-03 13:18:23 -0500

    Creando el ContentProvider, pt2

--- app/src/main/java/pe/com/bitsolutions/dailytime/timetracking/data/TareaContract.java
+++ app/src/main/java/pe/com/bitsolutions/dailytime/timetracking/data/TareaContract.java
@@ -1,11 +1,20 @@
 package pe.com.bitsolutions.dailytime.timetracking.data;
 
+import android.net.Uri;
 import android.provider.BaseColumns;
 
 /**
  * Created by Alberto on 10/3/2015.
  */
 public class TareaContract {
+
+    public static final String CONTENT_AUTHORITY = "pe.com.bitsolutions.dailytime";
+
+    public static final Uri BASE_CONTENT_URI = Uri.parse("content://" + CONTENT_AUTHORITY);
+
+    public static final String PATH_TAREA = "tarea";
+
+
 
     public static final class TareaEstructura implements BaseColumns{
 
@@ -16,5 +25,6 @@ public class TareaContract {
 
     }
 
+    // TODO crear las otras tablas
 
 }
